ae8ea4723e465e0257a6d62a3019a728767fff30
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-03-26 15:10:20 +0200

    Remove unused imports

--- src/main/java/proiect/DiscordListener.java
+++ src/main/java/proiect/DiscordListener.java
@@ -1,17 +1,8 @@
 package proiect;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Invite;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.Channel;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.internal.managers.channel.ChannelManagerImpl;
-import org.jetbrains.annotations.NotNull;
 
 public class DiscordListener extends ListenerAdapter {
     @Override
@@ -22,11 +13,9 @@ public class DiscordListener extends ListenerAdapter {
                 assert content != null; // Discord doesn't even allow you to call this command with an empty content but whatever ig
                 event.reply(content).queue();
             }
-            case "ping" -> {
-                event.reply("Pong!").queue();
-            }
+            case "ping" -> event.reply("Pong!").queue();
+
             default -> {
-                return;
             }
         }
     }
