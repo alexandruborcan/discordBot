3b4961738a79159e815a62c40c0809652261d6b7
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-03-26 15:08:56 +0200

    Remove unused function

--- src/main/java/proiect/DiscordListener.java
+++ src/main/java/proiect/DiscordListener.java
@@ -11,18 +11,15 @@ import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.internal.managers.channel.ChannelManagerImpl;
+import org.jetbrains.annotations.NotNull;
 
 public class DiscordListener extends ListenerAdapter {
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-
-    }
-
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         switch (event.getInteraction().getName()) {
             case "say" -> {
                 String content = event.getOption("content", OptionMapping::getAsString);
+                assert content != null; // Discord doesn't even allow you to call this command with an empty content but whatever ig
                 event.reply(content).queue();
             }
             case "ping" -> {
