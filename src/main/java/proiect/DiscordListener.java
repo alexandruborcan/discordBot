25b67655daf1a036476ae4ff98329494069212b1
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-03-26 14:46:18 +0200

    feat: Add Discord Integration
    - Updated `.idea/encodings.xml` to include `secrets.json` with UTF-8 encoding.
    - Added `.idea/inspectionProfiles/Project_Default.xml` to configure inspection profiles.
    - Added `.idea/jsonSchemas.xml` to include JSON Schema mappings for `secrets.json`.
    - Created `DiscordListener.java` to handle Discord bot events such as message reception and slash commands.
    - Modified `Main.java` to initialize the Discord bot with commands `say` and `ping`. Added exception handling for reading the Discord key from `SecretFileReader`.
    
    Changes:
    - `.idea/encodings.xml`: Added encoding for `secrets.json`.
    - `.idea/inspectionProfiles/Project_Default.xml`: New file for inspection profiles.
    - `.idea/jsonSchemas.xml`: New file for JSON Schema mappings.
    - `src/main/java/proiect/DiscordListener.java`: Implemented event handling for Discord bot.
    - `src/main/java/proiect/Main.java`: Integrated Discord bot initialization and command registration.

---
+++ src/main/java/proiect/DiscordListener.java
@@ -0,0 +1,36 @@
+package proiect;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Invite;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.internal.managers.channel.ChannelManagerImpl;
+
+public class DiscordListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        switch (event.getInteraction().getName()) {
+            case "say" -> {
+                String content = event.getOption("content", OptionMapping::getAsString);
+                event.reply(content).queue();
+            }
+            case "ping" -> {
+                event.reply("Pong!").queue();
+            }
+            default -> {
+                return;
+            }
+        }
+    }
+}
--- src/main/java/proiect/Main.java
+++ src/main/java/proiect/Main.java
@@ -1,17 +1,40 @@
 package proiect;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import java.io.IOException;
+
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import org.json.JSONException;
+
+import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String discordToken = null;
+        try {
+            discordToken = SecretFileReader.getDiscordKey();
+        } catch (IOException | JSONException e) {
+            System.err.println("Could not read Discord Key: " + e.getMessage());
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+            System.exit(1);
         }
+        JDA api = JDABuilder.createLight(discordToken, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(new DiscordListener())
+                .build();
+
+        // Register commands to make them visible globally on Discord:
+        CommandListUpdateAction commands = api.updateCommands();
+
+        // Add all your commands on this action instance
+        commands.addCommands(
+                Commands.slash("say", "Makes the bot say what you want")
+                        .addOption(STRING, "content", "What the bot should say", true), // Accepting a user input
+                Commands.slash("ping", "Makes the bot reply with Pong")
+        ).queue();
     }
 }
