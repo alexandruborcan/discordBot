ba1b64f2b6f5a851eee2eae2b264bb5535908e5a
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-04-03 17:15:05 +0300

    Add voice channel update handling to disconnect bot when alone

--- src/main/java/proiect/DiscordListener.java
+++ src/main/java/proiect/DiscordListener.java
@@ -1,7 +1,11 @@
 package proiect;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+import org.jetbrains.annotations.NotNull;
 
 public class DiscordListener extends ListenerAdapter {
 
@@ -39,6 +43,25 @@ public class DiscordListener extends ListenerAdapter {
             default:
                 event.reply("Unknown command.").queue();
                 break;
+        }
+    }
+
+    /**
+     * Handles the guild voice update events.
+     * This method gets called whenever there is an event in a voice channel.
+     *
+     * @param event the event triggered by a guild voice update.
+     */
+    @Override
+    public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
+        // If a user or a bot leaves any channel
+        if (event.getChannelLeft() != null) {
+            // If the bot is in the channel that the event occured, and the bot is alone, then disconnect.
+            if (event.getChannelLeft().getMembers().size() == 1) {
+                Member self = event.getGuild().getSelfMember();
+                AudioManager audioManager = self.getGuild().getAudioManager();
+                audioManager.closeAudioConnection();
+            }
         }
     }
 }
