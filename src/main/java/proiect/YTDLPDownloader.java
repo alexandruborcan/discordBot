0b6db1120d3dc3ea9eab860bd3bade5163914aad
DanielMazureac <daniel.mazureac@gmail.com>
2025-04-14 11:06:56 +0300

    Create class to handle YouTube downloads - detect operating system - set executable permission for yt-dlp, ffmpeg, and ffprobe - throw exception if executable permission fails - use ProcessBuilder to handle external process execution

--- src/main/java/proiect/YTDLPDownloader.java
+++ src/main/java/proiect/YTDLPDownloader.java
@@ -1,4 +1,54 @@
 package proiect;
 
+import java.io.*;
+
 public class YTDLPDownloader {
+    static String ytDlpPath;
+    static String ffmpegPath;
+
+    public static void runYtDlp(String videoUrl) throws IOException, InterruptedException {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            ytDlpPath = "bin/win/yt-dlp.exe";
+            ffmpegPath = "bin/win/ffmpeg.exe";
+
+        } else if (os.contains("mac")) {
+            ytDlpPath = "bin/mac/yt-dlp";
+            ffmpegPath = "bin/mac/ffmpeg";
+            if(!(new File("bin/mac/yt-dlp").setExecutable(true))){
+                throw new FileNotFoundException();
+            }
+            if(!(new File("bin/mac/ffmpeg").setExecutable(true))){
+                throw new FileNotFoundException();
+            }
+            if(!(new File("bin/mac/ffprobe").setExecutable(true))){
+                throw new FileNotFoundException();
+            }
+        } else {
+            ytDlpPath = "bin/linux/yt-dlp";
+            ffmpegPath = "bin/linux/ffmpeg";
+            if(!(new File("bin/linux/ffmpeg").setExecutable(true))){
+                throw new FileNotFoundException();
+            }
+            if(!(new File("bin/linux/ffprobe").setExecutable(true))){
+                throw new FileNotFoundException();
+            }
+        }
+
+        ProcessBuilder pb = new ProcessBuilder(ytDlpPath, "-x", "--ffmpeg-location", ffmpegPath, "--audio-format", "mp3",  videoUrl);
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line); // or handle programmatically
+            }
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("yt-dlp finished with exit code: " + exitCode);
+    }
 }
