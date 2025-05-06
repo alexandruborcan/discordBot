cadc86a7d340f2e4d4f71392388a718fe78105c1
Daniel Mazureac <daniel.mazureac@gmail.com>
2025-05-06 19:34:54 +0300

    Add Javadoc comments and minor code cleanup in YTDLPDownloader

--- src/main/java/proiect/YTDLPDownloader.java
+++ src/main/java/proiect/YTDLPDownloader.java
@@ -1,13 +1,26 @@
 package proiect;
 
 import java.io.*;
-import java.net.URISyntaxException;
 
+/**
+ * Utility class for downloading and converting YouTube videos to MP3 format using yt-dlp.
+ */
 public class YTDLPDownloader {
 
-    public static void runYtDlp(String videoUrl) throws IOException, InterruptedException, URISyntaxException {
+    /**
+     * Executes the yt-dlp process to download and convert a YouTube video to MP3 format.
+     *
+     * @param videoUrl the URL of the YouTube video to download
+     * @throws IOException if an I/O error occurs when starting or reading from the process
+     * @throws InterruptedException if the current thread is interrupted while waiting for the process to finish
+     */
+    public static void runYtDlp(String videoUrl) throws IOException, InterruptedException{
 
-        ProcessBuilder pb = new ProcessBuilder(Init.ytDlpPath, "-x", "--ffmpeg-location", Init.ffmpegPath, "--audio-format", "mp3",  videoUrl);
+        ProcessBuilder pb = new ProcessBuilder(
+                Init.ytDlpPath,
+                "-x", "--ffmpeg-location", Init.ffmpegPath,
+                "--audio-format", "mp3",
+                videoUrl);
         pb.redirectErrorStream(true);
         Process process = pb.start();
 
