44c36a3a62bcfe85154098f249f9a4224b673432
alexandruborcan <alexandru-vasile.borcan@student.tuiasi.ro>
2025-05-26 16:58:07 +0300

    feat: Add different download logic for links and titles, replace YoutubeDataAPI with yt-dlp integrated feature

--- src/main/java/proiect/YTDLPDownloader.java
+++ src/main/java/proiect/YTDLPDownloader.java
@@ -1,5 +1,7 @@
 package proiect;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
 import java.util.Random;
 
@@ -11,25 +13,16 @@ public class YTDLPDownloader {
     /**
      * Executes the yt-dlp process to download and convert a YouTube video to MP3 format.
      *
-     * @param videoUrl the URL of the YouTube video to download
+     * @param videoTitle the title of the YouTube video to download
      * @return The name of the file that was downloaded
      * @throws IOException if an I/O error occurs when starting or reading from the process
      * @throws InterruptedException if the current thread is interrupted while waiting for the process to finish
      */
-    public static String runYtDlp(String videoUrl) throws IOException, InterruptedException{
+    public static String runYtDlp(String videoTitle, boolean link) throws IOException, InterruptedException{
         Random rand = new Random();
         String absoluteTime = String.valueOf(System.currentTimeMillis());
         String fileName = "yt-dlp-" + absoluteTime + rand.nextInt() + ".mp3";
-        ProcessBuilder pb = new ProcessBuilder(
-                Initialize.ytDlpPath,
-                "-x", "--ffmpeg-location", Initialize.ffmpegPath,
-                "--embed-metadata",
-                "--audio-format", "mp3",
-                "-S", "+size", // smallest size available
-                "--output", fileName,
-                videoUrl);
-        pb.redirectErrorStream(true);
-        Process process = pb.start();
+        Process process = getProcess(videoTitle, link, fileName);
 
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(process.getInputStream()))) {
@@ -42,5 +35,33 @@ public class YTDLPDownloader {
         int exitCode = process.waitFor();
         System.out.println("yt-dlp finished with exit code: " + exitCode);
         return fileName;
+    }
+
+    private static @NotNull Process getProcess(String videoTitle, boolean link, String fileName) throws IOException {
+        ProcessBuilder pb;
+        if (link){
+            pb = new ProcessBuilder(
+                    Initialize.ytDlpPath,
+                    "-x", "--ffmpeg-location", Initialize.ffmpegPath,
+                    "--embed-metadata",
+                    "--audio-format", "mp3",
+                    "-S", "+size", // smallest size available
+                    "--output", fileName,
+                    "--default-search", "ytsearch",
+                    videoTitle);
+        }
+        else{
+            pb = new ProcessBuilder(
+                    Initialize.ytDlpPath,
+                    "-x", "--ffmpeg-location", Initialize.ffmpegPath,
+                    "--embed-metadata",
+                    "--audio-format", "mp3",
+                    "-S", "+size", // smallest size available
+                    "--output", fileName,
+                    videoTitle);
+        }
+
+        pb.redirectErrorStream(true);
+        return pb.start();
     }
 }
